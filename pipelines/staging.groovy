e112f767f14c7e4ff089a6e19de4a0b1ddda1c4a
donkey <84121166+iadonkey@users.noreply.github.com>
2022-01-09 10:33:33 +0100

    Update staging.groovy

--- pipelines/staging.groovy
+++ pipelines/staging.groovy
@@ -25,10 +25,13 @@ pipeline {
 			
     stage('Build') {
       steps {
+        bat "cmdkey /generic:TERMSRV/werkstatt2 /user:zeugwerker /pass:1"
+        bat "mstsc /v:werkstatt2 /w:640 /h:480"         
         bat "nuget.exe restore"
-	dir('bin') { deleteDir() }
-	bat "mkdir bin"
-        bat "Godot.exe --no-window --export \"Windows Desktop\" \"${WORKSPACE}\\bin\\Tutorial_Quickstart_Visualization.exe\""  
+        dir('bin') { deleteDir() }
+        bat "mkdir bin"
+        bat "Godot.exe --no-window --export \"Windows Desktop\" \"${WORKSPACE}\\bin\\Tutorial_Quickstart_Visualization.exe\""
+        bat "cmdkey /delete:TERMSRV/werkstatt2"
         dir('bin') {
           archiveArtifacts artifacts: '**'
         }
