59247a6da2ea8b151dfe54e5e8e9b110055d7f3d
Stefan Besler <84121166+beslst@users.noreply.github.com>
2026-03-09 12:06:36 +0100

    Update staging.groovy

--- pipelines/staging.groovy
+++ pipelines/staging.groovy
@@ -26,7 +26,7 @@ pipeline {
           dir("bindings/Tutorial_Quickstart") { 
             currentBuild.upstreamBuilds?.each { b -> 
               println "CopyArtifacts from ${b.getFullProjectName()}"
-              copyArtifacts filter: 'bindings/Zeugwerk Quickstart/Classes.cs,bindings/Zeugwerk Quickstart/Enums.cs,bindings/Zeugwerk Quickstart/Structs.cs', projectName: b.getFullProjectName() as String, selector: upstream(), target: '.', flatten: true, optional: false 
+              copyArtifacts filter: 'bindings/Zeugwerk_Quickstart/Classes.cs,bindings/Zeugwerk_Quickstart/Enums.cs,bindings/Zeugwerk_Quickstart/Structs.cs', projectName: b.getFullProjectName() as String, selector: upstream(), target: '.', flatten: true, optional: false 
             }
           }
           
